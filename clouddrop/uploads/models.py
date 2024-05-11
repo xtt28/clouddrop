3b52c1e91afd5fa2227ea519fe95d3f9a8843235
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 12:13:46 -0400

    Add `__str__` method to `FileUpload` model

--- clouddrop/uploads/models.py
+++ clouddrop/uploads/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+import os
 
 def user_directory_path(instance, filename):
     return f'user_{instance.user.id}/{filename}'
@@ -8,3 +9,6 @@ class FileUpload(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     data = models.FileField(upload_to=user_directory_path)
+
+    def __str__(self):
+        return f'File upload {os.path.basename(self.data.name)}'
