406094063263f1dade796cc3a586163ca48d1d70
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 12:32:52 -0400

    Add field for file upload visibility

--- clouddrop/uploads/models.py
+++ clouddrop/uploads/models.py
@@ -6,9 +6,10 @@ def user_directory_path(instance, filename):
     return f'user_{instance.user.id}/{filename}'
 
 class FileUpload(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="uploads")
     created_at = models.DateTimeField(auto_now_add=True)
     data = models.FileField(upload_to=user_directory_path)
+    public = models.BooleanField(default=False)
 
     def __str__(self):
         return f'File upload {os.path.basename(self.data.name)}'
