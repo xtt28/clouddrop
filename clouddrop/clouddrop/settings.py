a44a0fd7f3b26eb7d05ac42434216bdc06657430
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 12:00:59 -0400

    Create custom user model `CUser`

--- clouddrop/clouddrop/settings.py
+++ clouddrop/clouddrop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Settings added by CloudDrop application
+
+AUTH_USER_MODEL = 'users.CUser'
