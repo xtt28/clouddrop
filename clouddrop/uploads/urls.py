98b7a9eff71401be99ebf2461438151f7bd2e419
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 13:31:24 -0400

    Add file upload creation form

--- clouddrop/uploads/urls.py
+++ clouddrop/uploads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FileUploadListView
+from .views import FileUploadCreateView, FileUploadListView
 
 app_name = 'uploads'
 urlpatterns = [
-    path('', FileUploadListView.as_view(), name='list')
+    path('', FileUploadListView.as_view(), name='list'),
+    path('create', FileUploadCreateView.as_view(), name='create')
 ]
--- clouddrop/uploads/views.py
+++ clouddrop/uploads/views.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from .models import FileUpload
 
 class FileUploadListView(LoginRequiredMixin, ListView):
     model = FileUpload
     paginate_by = 20
-    context_object_name = "uploads"
-    ordering = "-created_at"
+    context_object_name = 'uploads'
+    ordering = '-created_at'
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
+
+
+class FileUploadCreateView(LoginRequiredMixin, CreateView):
+    model = FileUpload
+    fields = ('data', 'public')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.save()
+        return super().form_valid(form)
