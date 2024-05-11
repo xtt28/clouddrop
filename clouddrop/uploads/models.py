2037835e080fc4d9e41bbceb28da8648622d9651
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 12:06:23 -0400

    Add model `FileUpload` and install uploads app

--- clouddrop/uploads/models.py
+++ clouddrop/uploads/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    return f'user_{instance.user.id}/{filename}'
+
+class FileUpload(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    data = models.FileField(upload_to=user_directory_path)
