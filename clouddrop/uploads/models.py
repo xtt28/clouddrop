540326bda417b56f3b416baba6fb04ba1180a355
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 16:19:54 -0400

    Prepend current Unix time to file names when storing

--- clouddrop/uploads/models.py
+++ clouddrop/uploads/models.py
@@ -1,9 +1,10 @@
 import os
+import time
 from django.contrib.auth import get_user_model
 from django.db import models
 
 def user_directory_path(instance, filename):
-    return f'user_{instance.user.id}/{filename}'
+    return f'user_{instance.user.id}/{int(time.time())}.{filename}'
 
 class FileUpload(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="uploads")
