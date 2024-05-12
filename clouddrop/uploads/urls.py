48b3b46ae51451900985084bb5751b2a5eae540a
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-12 07:53:51 -0400

    Add downloading functionality for files

--- clouddrop/uploads/urls.py
+++ clouddrop/uploads/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import FileUploadCreateView, FileUploadDetailView, FileUploadListView
+from .views import (
+    FileUploadCreateView,
+    FileUploadDetailView,
+    FileUploadDownloadView,
+    FileUploadListView,
+)
 
 app_name = 'uploads'
 urlpatterns = [
     path('', FileUploadListView.as_view(), name='list'),
     path('create', FileUploadCreateView.as_view(), name='create'),
     path('<int:pk>', FileUploadDetailView.as_view(), name='detail'),
+    path('<int:pk>/download', FileUploadDownloadView.as_view(), name='download'),
 ]
--- clouddrop/uploads/views.py
+++ clouddrop/uploads/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from .models import FileUpload
+
 
 class FileUploadListView(LoginRequiredMixin, ListView):
     model = FileUpload
@@ -23,11 +27,23 @@ class FileUploadCreateView(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         form.instance.save()
         return super().form_valid(form)
-    
+
 
 class FileUploadDetailView(DetailView):
     model = FileUpload
     context_object_name = 'upload'
 
     def get_queryset(self):
-        return super().get_queryset().filter(Q(public=True) | Q(user=self.request.user.id))
+        return (
+            super().get_queryset().filter(Q(public=True) | Q(user=self.request.user.id))
+        )
+
+
+class FileUploadDownloadView(View):
+    def get(self, request, pk):
+        upload = get_object_or_404(
+            FileUpload.objects.filter(id=pk).filter(
+                Q(public=True) | Q(user=request.user.id)
+            )
+        )
+        return FileResponse(upload.data.open('rb'), as_attachment=True)
