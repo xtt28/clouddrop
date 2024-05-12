97277ae05f12902e370e2229ba79524a64d5a470
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-11 20:09:59 -0400

    Add `get_absolute_url` method to `FileUpload` model

--- clouddrop/uploads/models.py
+++ clouddrop/uploads/models.py
@@ -2,15 +2,19 @@ import os
 import time
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 def user_directory_path(instance, filename):
     return f'user_{instance.user.id}/{int(time.time())}.{filename}'
 
 class FileUpload(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="uploads")
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='uploads')
     created_at = models.DateTimeField(auto_now_add=True)
     data = models.FileField(upload_to=user_directory_path)
     public = models.BooleanField(default=False)
 
     def __str__(self):
         return f'File upload {os.path.basename(self.data.name)}'
+
+    def get_absolute_url(self):
+        return reverse('uploads:detail', kwargs={"pk": self.pk})
