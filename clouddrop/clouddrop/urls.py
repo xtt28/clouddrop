8800c9479338ef360ffa79530ac90789152d622e
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-13 17:23:49 -0400

    Add landing page

--- clouddrop/clouddrop/urls.py
+++ clouddrop/clouddrop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('uploads/', include('uploads.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', TemplateView.as_view(template_name="home.html"))
 ]
